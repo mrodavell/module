06fceaf808337c6011f099d65c5199b2eb5effb8
mrodavell <jomar.llevado@ustp.edu.ph>
2020-11-09 20:28:15 +0800

    For Initial Production Release

---
+++ examinee/precache-manifest.d8342add2b376ff16e652ea60c3a2991.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b4d6c444b6cda4426fd5cc9e17b595",
+    "url": "/prc/module/examinee/index.html"
+  },
+  {
+    "revision": "8f0dc7f361867c009f42",
+    "url": "/prc/module/examinee/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1386eaa1d5d8b0c33603",
+    "url": "/prc/module/examinee/static/css/main.43383ca0.chunk.css"
+  },
+  {
+    "revision": "8f0dc7f361867c009f42",
+    "url": "/prc/module/examinee/static/js/2.9387ca07.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/prc/module/examinee/static/js/2.9387ca07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1386eaa1d5d8b0c33603",
+    "url": "/prc/module/examinee/static/js/main.05b98192.chunk.js"
+  },
+  {
+    "revision": "a34558ef19fbd45a77fb",
+    "url": "/prc/module/examinee/static/js/runtime-main.9380923c.js"
+  },
+  {
+    "revision": "bdddb4d4999ce1cb8ce56e4e0bf50b83",
+    "url": "/prc/module/examinee/static/media/bg.bdddb4d4.jpg"
+  },
+  {
+    "revision": "0dad67f1333e8b775dad390ccf2c6b79",
+    "url": "/prc/module/examinee/static/media/logo.0dad67f1.png"
+  },
+  {
+    "revision": "a6cbd833e4f2ce6fd49fafeb67000fe4",
+    "url": "/prc/module/examinee/static/media/prc.a6cbd833.png"
+  }
+]);
